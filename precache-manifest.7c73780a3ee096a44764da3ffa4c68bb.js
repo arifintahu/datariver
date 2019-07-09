a762b2a72a399d6b946b884a825d985f717c91a9
Miftahul Arifin <miftahulfin@gmail.com>
2019-07-09 22:13:54 +0700

    Updates

---
+++ precache-manifest.7c73780a3ee096a44764da3ffa4c68bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669716e515f138083d0e43f3e2fadbea",
+    "url": "/datariver/index.html"
+  },
+  {
+    "revision": "ede4c48c6bb12d9df97c",
+    "url": "/datariver/static/css/main.353e6cd8.chunk.css"
+  },
+  {
+    "revision": "2dfbe02a621566c24735",
+    "url": "/datariver/static/js/2.634f637c.chunk.js"
+  },
+  {
+    "revision": "ede4c48c6bb12d9df97c",
+    "url": "/datariver/static/js/main.81d9ca0e.chunk.js"
+  },
+  {
+    "revision": "8389df470e30a10f28eb",
+    "url": "/datariver/static/js/runtime~main.1a347e1a.js"
+  }
+]);
